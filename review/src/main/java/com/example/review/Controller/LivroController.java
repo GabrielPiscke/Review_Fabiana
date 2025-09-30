d971ac582d604e9b86da051d69263ecec3d52e69
= <=>
2025-09-30 15:03:56 -0300

    controller começo

--- review/src/main/java/com/example/review/Controller/LivroController.java
+++ review/src/main/java/com/example/review/Controller/LivroController.java
@@ -1,10 +1,20 @@
 package com.example.review.Controller;
 
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import com.example.review.Entity.Livro;
+import com.example.review.Service.LivroService;
+
 @RestController
-@RequestMapping
+@RequestMapping("/livros")
 public class LivroController {
+
+    @Autowired
+    private LivroService service;   
+
+ 
 
 }
