dd434bbf85baa8dc4c948a2d72f5834b7f234182
GabrielPiscke <gabrielmateuspiscke@gmail.com>
2025-09-30 14:08:51 -0300

    Up2

---
+++ review/src/main/java/com/example/review/Entity/Livro.java
@@ -0,0 +1,15 @@
+package com.example.review.Entity;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class Livro {
+    private
+    private
+    private
+    private
+}
